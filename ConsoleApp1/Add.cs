b8d146c53c6ac95d480ce3fd08b036d94ee2e81d
Lallu Chaitanya <lalluthiramdas@gmail.com>
2026-06-11 13:48:11 -0500

    initial commit

---
+++ ConsoleApp1/Add.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class Add
+{
+    public static void Run()
+    {
+        Console.Write("Enter first number: ");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter second number: ");
+        int num2 = Convert.ToInt32(Console.ReadLine());
+
+        int sum = num1 + num2;
+
+        Console.WriteLine("Sum = " + sum);
+
+        Console.ReadLine();
+    }
+}
